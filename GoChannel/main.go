ddba5ab2f237fd1ddf5c4b21e9fd9a1c47ca3f2a
Subhasis <subhasispatra94@gmail.com>
2021-12-03 01:36:39 +0530

    Added a Go Channel example

--- GoChannel/main.go
+++ GoChannel/main.go
@@ -1,7 +1,38 @@
-package main 
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
-func main(){
-	fmt.Println("Go Channels will be impimentd witht the goRoutins ... ")
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "is down")
+		c <- "might be down"
+		return
+	}
+	fmt.Println(link, "is active")
+	c <- "it up !"
+}
+
+func main() {
+	fmt.Println("Go Channels  ... ")
+	links := []string{
+		"http://amazon.com",
+		"http://google.com",
+		"http://flipkart.com",
+		"http://walmart.com",
+		"http://facebook.com",
+	}
+	// create a cahnnel
+	c := make(chan string)
+
+	for _, val := range links {
+		go checkLink(val, c)
+		// fmt.Println(<-c)
+	}
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<-c)
+	}
 }
